b7811f9f8c8dd558164bf6b7d42b70de158981fe
JENISH SUKHADIYA <jenishsukhadiya1005@gmail.com>
2023-01-07 14:56:35 +0530

    assessment.c

---
+++ assesment C.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+main()
+{
+	int  equality,stringcopy,contant,show,reverse,palidrome,sub,string,exit,num;
+	char ch;
+
+    
+   
+	{
+		printf("main manu");
+		printf("\n\t 1.equality");
+		printf("\n\t 2.stringcopy");
+		printf("\n\t 3.contant");
+		printf("\n\t 4.show");
+		printf("\n\t 5.reverse");
+		printf("\n\t 6.palidrome");
+		printf("\n\t 7.sub");
+		printf("\n\t 8.string");
+		printf("\n\t 9.exit");
+		
+		printf("\n\nenter your choice=");
+		scanf("%d",&num);
+		
+		
+		switch(num)
+		{
+			case 1:
+			printf("please enter the string =");
+			printf("\n equality");
+			
+			break;
+			
+			case 2:
+			printf("please enter the string =");
+			printf(" \nstringcopy");
+			
+			break;
+			
+			case 3:
+			printf("please enter the string =");
+			printf(" contant \n");
+			
+			break;
+			
+			case 4:
+			printf("please enter the string =");
+			printf(" show\n");
+		
+			break;
+			
+			case 5:
+			printf("please enter the string =");
+			printf(" reverse\n");
+			
+			break;
+			
+			case 6:
+			printf("please enter the string =");
+			printf(" palidrome\n");
+			
+			break;
+			
+			case 7:
+			printf("please enter the string =");
+			printf(" sub\n");
+			
+			break;
+			
+			case 8:
+			printf("please enter the string =");
+			printf(" string\n");
+			
+			break;
+			
+			case 9:
+			printf("please enter the string =");
+			printf("exit\n");
+			
+			break;
+			
+			default :
+		printf("better luck next time");
+			
+		}
+			
+	while((ch = getchar())!=EOF)
+	printf("enter the text.press cntrl Z:");
+		
+   
+     printf("main manu");
+		printf("\n\t 1.equality");
+		printf("\n\t 2.stringcopy");
+		printf("\n\t 3.contant");
+		printf("\n\t 4.show");
+		printf("\n\t 5.reverse");
+		printf("\n\t 6.palidrome");
+		printf("\n\t 7.sub");
+		printf("\n\t 8.string");
+		printf("\n\t 9.exit");
+
+	
+  }
+}
